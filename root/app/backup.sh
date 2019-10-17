07020179a0eb07b6325b99be3c74f2dd75f4dc56
Alexander Zinchenko <alexander@zinchenko.com>
2019-10-17 08:46:28 +0300

    Implement sending log by email

--- root/app/backup.sh
+++ root/app/backup.sh
@@ -19,42 +19,80 @@ converts()
    fi
 }
 
-echo ========== Run backup job at `date` ==========
+log_dir=""
+log_file=/dev/null
+mail_file=/dev/null
+
+if [[ ! -z ${EMAIL_SMTP_SERVER} ]] && [[ ! -z ${EMAIL_RECIPIENT_EMAIL} ]]; then
+    log_dir=`mktemp -d`
+    log_file=$log_dir/backup.log
+    mail_file=$log_dir/mailbody.log
+fi
+
+echo ========== Run backup job at `date` ========== | tee $log_file
 
 start=$(date +%s.%N)
 config_dir=/config
 
 cd $config_dir
 
-echo "*** Backup ***"
-duplicacy backup -threads $THREADS_NUM
+echo "*** Backup ***" | tee -a $log_file
+duplicacy backup -threads $THREADS_NUM | tee -a $log_file
 exitcode=$?
 
-if [[ $exitcode -eq 0 ]] && [[ ! -z "${PRUNE_KEEP_POLICIES}" ]]; then
+if [[ $exitcode -eq 0 ]] && [[ ! -z ${PRUNE_KEEP_POLICIES} ]]; then
     IFS=';'
-    read -ra policies <<< "$PRUNE_KEEP_POLICIES"
+    read -ra policies <<< $PRUNE_KEEP_POLICIES
     command=""
-    for policy in "${policies[@]}"; do
+    for policy in ${policies[@]}; do
         command="$command -keep $policy"
     done
 
-    echo "*** Prune chunks by policies ***"
-    sh -c "duplicacy prune $command -threads $THREADS_NUM"
+    echo "*** Prune chunks by policies ***" | tee -a $log_file
+    sh -c "duplicacy prune $command -threads $THREADS_NUM" | tee -a $log_file
     exitcode=$?
 
     if [[ $exitcode -eq 0 ]]; then
-        echo "*** Delete marked chunks ***"
-        duplicacy prune -exhaustive -threads $THREADS_NUM
+        echo "*** Delete marked chunks ***" | tee -a $log_file
+        duplicacy prune -exhaustive -threads $THREADS_NUM | tee -a $log_file
         exitcode=$?
     fi
 fi
 
 duration=$(echo "$(date +%s.%N) - $start" | bc)
+subject=""
 
-if [ $exitcode -ne 0 ]; then
-    echo Backup FAILED, code $exitcode, lasted $(converts $duration)
+if [ $exitcode -eq 0 ]; then
+    echo Backup COMPLETED, total time $(converts $duration) | tee -a $log_file
+    subject="duplicacy job COMPLETED, id $SNAPSHOT_ID url $STORAGE_URL"
 else
-    echo Backup COMPLETED, lasted $(converts $duration)
+    echo Backup FAILED, code $exitcode, total time $(converts $duration) | tee -a $log_file
+    subject="duplicacy job FAILED, id $SNAPSHOT_ID url $STORAGE_URL"
+fi
+
+if [[ ! -z ${EMAIL_SMTP_SERVER} ]] && [[ ! -z ${EMAIL_RECIPIENT_EMAIL} ]]; then
+    echo "To: $EMAIL_RECIPIENT_EMAIL" >> $mail_file
+    echo "Subject: $subject" >> $mail_file
+    echo "" >> $mail_file
+
+    if [[ `wc -l $log_file | awk '{ print $1 }'` -gt $((2*$EMAIL_LOG_LINES_IN_BODY)) ]]; then
+        head -$EMAIL_LOG_LINES_IN_BODY $log_file >> $mail_file
+        echo "..." >> $mail_file
+        tail -n $EMAIL_LOG_LINES_IN_BODY $log_file >> $mail_file
+    else
+        cat $log_file >> $mail_file
+    fi
+
+    zip_log_file=$log_dir/backuplog.zip
+    zipout=`zip -j $zip_log_file $log_file`
+
+    if [ $exitcode -ne 0 ]; then
+        echo $zipout
+    fi
+
+    cat $mail_file | (cat - && uuencode $zip_log_file backuplog.zip) | ssmtp -F "" $EMAIL_RECIPIENT_EMAIL
+
+    rm -rf $log_dir
 fi
 
 exit $exitcode

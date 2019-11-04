f627eb4f023a64c5ad39d243697d022ac3689a9f
Alexander Zinchenko <alexander@zinchenko.com>
2019-11-04 13:03:51 +0300

    Revert MTS to ssmtp, create attachment manually

--- root/app/backup.sh
+++ root/app/backup.sh
@@ -77,6 +77,15 @@ else
 fi
 
 if [[ ! -z ${EMAIL_SMTP_SERVER} ]] && [[ ! -z ${EMAIL_TO} ]]; then
+    boundary="_====_boundary_====_$(date +%Y%m%d%H%M%S)_====_"
+
+    echo "To: $EMAIL_TO" >> $mail_file
+    echo "Subject: $subject" >> $mail_file
+    echo "Content-Type: multipart/mixed; boundary=\"$boundary\"" >> $mail_file
+    echo "Mime-Version: 1.0" >> $mail_file
+    echo "" >> $mail_file
+    echo "--${boundary}" >> $mail_file
+
     if [[ `wc -l $log_file | awk '{ print $1 }'` -gt $((2*$EMAIL_LOG_LINES_IN_BODY)) ]]; then
         head -$EMAIL_LOG_LINES_IN_BODY $log_file >> $mail_file
         echo "..." >> $mail_file
@@ -92,7 +101,17 @@ if [[ ! -z ${EMAIL_SMTP_SERVER} ]] && [[ ! -z ${EMAIL_TO} ]]; then
         echo $zipout
     fi
 
-    mutt -a $zip_log_file -s "$subject" -- $EMAIL_TO < $mail_file
+    echo "" >> $mail_file
+    echo "--${boundary}" >> $mail_file
+    echo "Content-Transfer-Encoding: base64" >> $mail_file
+    echo "Content-Type: application/octet-stream; name=backuplog.zip" >> $mail_file
+    echo "Content-Disposition: attachment; filename=backuplog.zip" >> $mail_file
+    echo "" >> $mail_file
+    base64 $zip_log_file >> $mail_file
+    echo "" >> $mail_file
+    echo "--${boundary}--" >> $mail_file
+
+    cat $mail_file | ssmtp -F "" $EMAIL_TO
 
     rm -rf $log_dir
 fi

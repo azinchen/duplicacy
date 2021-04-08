2de629e62c11a1a719fc25f24ac3b8fe01b66926
Jonathan Stucklen <john@goodpath.com>
2021-04-08 11:59:06 -0400

    Don't run the backup if the pre backup script fails.

--- root/app/backup.sh
+++ root/app/backup.sh
@@ -22,38 +22,45 @@ if [[ ! -z ${PRE_BACKUP_SCRIPT} ]]; then
         echo Run pre backup script | tee -a $log_file
         export log_file my_dir # Variables I require in my pre backup script
         sh -c "${PRE_BACKUP_SCRIPT}"
+        exitcode = ${PIPESTATUS[0]
     else
         echo Pre backup script defined, but file not found | tee -a $log_file
+        # Command not found exit code (https://tldp.org/LDP/abs/html/exitcodes.html)
+        exitcode = 127
     fi
 fi
-
-start=$(date +%s.%N)
-config_dir=/config
-
-cd $config_dir
-
-nice -n $PRIORITY_LEVEL duplicacy $GLOBAL_OPTIONS backup $BACKUP_OPTIONS | tee -a $log_file
-exitcode=${PIPESTATUS[0]}
-
-duration=$(echo "$(date +%s.%N) - $start" | bc)
-subject=""
 
 if [ $exitcode -eq 0 ]; then
-    echo Backup COMPLETED, duration $(converts $duration) | tee -a $log_file
-    subject="duplicacy backup job id \"$hostname:$SNAPSHOT_ID\" COMPLETED"
-else
-    echo Backup FAILED, code $exitcode, duration $(converts $duration) | tee -a $log_file
-    subject="duplicacy backup job id \"$hostname:$SNAPSHOT_ID\" FAILED"
-fi
+    start=$(date +%s.%N)
+    config_dir=/config
 
-if [[ ! -z ${POST_BACKUP_SCRIPT} ]]; then
-    if [[ -f ${POST_BACKUP_SCRIPT} ]]; then
-        echo Run post backup script | tee -a $log_file
-        export log_file exitcode duration my_dir # Variables I require in my post backup script
-        sh -c "${POST_BACKUP_SCRIPT}"
+    cd $config_dir
+
+    nice -n $PRIORITY_LEVEL duplicacy $GLOBAL_OPTIONS backup $BACKUP_OPTIONS | tee -a $log_file
+    exitcode=${PIPESTATUS[0]}
+
+    duration=$(echo "$(date +%s.%N) - $start" | bc)
+    subject=""
+
+    if [ $exitcode -eq 0 ]; then
+        echo Backup COMPLETED, duration $(converts $duration) | tee -a $log_file
+        subject="duplicacy backup job id \"$hostname:$SNAPSHOT_ID\" COMPLETED"
     else
-        echo Post backup script defined, but file not found | tee -a $log_file
+        echo Backup FAILED, code $exitcode, duration $(converts $duration) | tee -a $log_file
+        subject="duplicacy backup job id \"$hostname:$SNAPSHOT_ID\" FAILED"
     fi
+
+    if [[ ! -z ${POST_BACKUP_SCRIPT} ]]; then
+        if [[ -f ${POST_BACKUP_SCRIPT} ]]; then
+            echo Run post backup script | tee -a $log_file
+            export log_file exitcode duration my_dir # Variables I require in my post backup script
+            sh -c "${POST_BACKUP_SCRIPT}"
+        else
+            echo Post backup script defined, but file not found | tee -a $log_file
+        fi
+    fi
+else
+    echo Pre backup script FAILED, code $exitcode, | tee -a $log_file
 fi
 
 "$my_dir/mailto.sh" $log_dir "$subject"

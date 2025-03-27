bc8ddf57eb51fcc746e9759d50ea4941d82652e2
adripo <26493496+adripo@users.noreply.github.com>
2025-03-27 07:06:47 +0100

    Fix: update filters on restart from ENV (#330)
    * fix: remove init check
    
    * fix: re-init filters file
    
    * fix: operation order
    
    * fix: correct quotes in duplicacy command

--- root/usr/bin/container-init-up-20-init.sh
+++ root/usr/bin/container-init-up-20-init.sh
@@ -3,15 +3,22 @@
 config_dir=/config
 cd "$config_dir" || exit 128
 
-if [[ -f .duplicacy ]]; then
-    echo "This folder has already been initialized with duplicacy. Not initializing again"
-    exit 0
-fi
-
 params=""
 config_dir=/config
 data_dir=/data
 filters_file=$config_dir/filters
+
+if [[ -n "${FILTER_PATTERNS}" ]]; then
+    if [[ -f $filters_file ]]; then
+        rm -f $filters_file
+    fi
+    
+    IFS=';'
+    read -ra filters <<< "$FILTER_PATTERNS"
+    for filter in "${filters[@]}"; do
+        echo "$filter" >> $filters_file
+    done
+fi
 
 if [[ -n ${DUPLICACY_PASSWORD} ]]; then
     params=-e
@@ -35,19 +42,11 @@ fi
 
 params="$params -pref-dir $config_dir -repository $data_dir $SNAPSHOT_ID $STORAGE_URL"
 
-duplicacy $GLOBAL_OPTIONS init $params
+eval duplicacy $GLOBAL_OPTIONS init $params
 exitcode=$?
 
 if [ $exitcode -ne 0 ]; then
     exit $exitcode
-fi
-
-if [[ ! -f $filters_file ]] && [[ -n "${FILTER_PATTERNS}" ]]; then
-    IFS=';'
-    read -ra filters <<< "$FILTER_PATTERNS"
-    for filter in "${filters[@]}"; do
-        echo "$filter" >> $filters_file
-    done
 fi
 
 exit 0
